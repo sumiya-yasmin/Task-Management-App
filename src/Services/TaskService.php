af74fcd39c7971ae7f6dac5927b71bc068c362b3
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-02-01 23:17:02 +0600

    Implement task service

---
+++ src/Services/TaskService.php
@@ -0,0 +1,47 @@
+<?php
+namespace App\TaskManager\Services;
+use App\TaskManager\Models\Task;
+use App\TaskManager\Interfaces\TaskRepositoryInterface;
+use RunTimeException;
+class TaskService{
+    private TaskRepositoryInterface $repository;
+    public function createTask(string $title): Task{
+        $task = new Task($title);
+        $this->repository->save($task);
+        return $task;
+    }
+    public function markTaskAsDone(int $id):void{
+         $task=$this->repository->findById($id);
+         if(!$task){
+            throw new RunTimeException("Task not found");
+         }
+         $task->markAsDone();
+         $this->repository->save($task);
+    }
+    public function cancelTask(int $id):void{
+        $task=$this->repository->findById($id);
+        if(!$task){
+            throw new RunTimeException("Task not found");
+         }
+         $task->cancel();
+         $this->repository->save($task);
+    }
+    public function deleteTask(int $id): void{
+        $task=$this->repository->findById($id);
+         if(!$task){
+            throw new RunTimeException("Task not found");
+         }
+         $this->repository->delete($task);
+    }
+    public function getTask(int $id):?Task{
+       return $this->repository->findById($id);
+    }
+    public function getAllTasks(): array{
+        return $this->repository->findAll();
+    }
+
+    public function __construct(TaskRepositoryInterface $repository){
+         $this->repository = $repository;
+
+    }
+}
