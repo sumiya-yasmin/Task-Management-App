e212c665751b0de2399d349ca04060d57623f2f7
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-02-03 14:50:30 +0600

    Add starting and conditional complete date

--- src/Views/tasks.php
+++ src/Views/tasks.php
@@ -10,7 +10,7 @@
 </head>
 
 <body class="bg-gray-100 min-h-screen py-10">
-    <div class="max-w-2xl mx-auto bg-white shadow-xl rounded-xl overflow-hidden">
+    <div class="max-w-4xl mx-auto bg-white shadow-xl rounded-xl overflow-hidden">
         <header class="bg-blue-600 p-6 text-white text-center">
             <h1 class="text-3xl font-bold">Task Manager</h1>
             <p class="text-blue-100 mt-1">Organize your workflow</p>
@@ -29,36 +29,47 @@
                     <p class="text-center text-gray-500 py-10">No Tasks Yet. Add One Above!</p>
                 <?php endif; ?>
                 <?php foreach ($tasks as $task): ?>
-                    <div class="flex items-center justify-between p-4 border rounded-lg hover:bg-gray-50 transition shadow-sm">
-                        <div class="flex gap-4 items-center justify-center ">
-                            <span><?php echo $task->getTitle(); ?></span>
-                            <?php if ($task->getStatus()->value === 'ongoing'): ?>
-                                <span class="border-2 border-gray-300 bg-gray-100 p-1 rounded-lg text-orange-300"> <?php echo $task->getStatus()->value ?> </span>
-                            <?php endif; ?>
-                        </div>
-                        <div class="flex justify-between gap-1">
-                            <?php if ($task->getStatus()->value === 'ongoing'): ?>
-                                <div class="hover:bg-gray-100 p-2 rounded-lg">
-                                    <a href="index.php?action=done&id=<?php echo $task->getId(); ?>">
-                                        <i data-lucide="check" class="w-4 h-4 text-green-700 stroke-[3px]"></i>
-                                    </a>
-                                </div>
-                                <div class="hover:bg-gray-100 p-2 rounded-lg">
-                                    <a href="index.php?action=cancel&id=<?php echo $task->getId(); ?>">
-                                        <i data-lucide="X" class="w-4 h-4 text-red-700 stroke-[4px]"></i>
-                                    </a>
-                                </div>
-
-                            <?php else: ?>
-                                <div class="border-2 border-gray-300 bg-gray-100 p-1 rounded-lg <?php echo ($task->getStatus()->value === 'done') ? 'text-green-500' : 'text-red-500' ?>">
-                                    <?php echo $task->getStatus()->value ?>
-                                </div>
-                            <?php endif; ?>
-                            <div class="hover:bg-red-100 p-2 rounded-lg">
-                                <a href="index.php?action=delete&id=<?php echo $task->getId(); ?>">
-                                    <i data-lucide="trash" class="w-4 h-4 text-black-700 stroke-[3px]"></i>
-                                </a>
+                    <div class="flex flex-col gap-2 p-4 border rounded-lg hover:bg-gray-50 transition shadow-sm">
+                        <div class="flex items-center justify-between gap-2">
+                            <div class="flex gap-4 items-center justify-center ">
+                                <span><?php echo $task->getTitle(); ?></span>
+                                <?php if ($task->getStatus()->value === 'ongoing'): ?>
+                                    <span class="border-2 border-gray-300 bg-gray-100 p-1 rounded-lg text-orange-300"> <?php echo $task->getStatus()->value ?> </span>
+                                <?php endif; ?>
                             </div>
+
+
+
+                            <div class="flex justify-between gap-1">
+                                <?php if ($task->getStatus()->value === 'ongoing'): ?>
+                                    <div class="hover:bg-gray-100 p-2 rounded-lg">
+                                        <a href="index.php?action=done&id=<?php echo $task->getId(); ?>">
+                                            <i data-lucide="check" class="w-4 h-4 text-green-700 stroke-[3px]"></i>
+                                        </a>
+                                    </div>
+                                    <div class="hover:bg-gray-100 p-2 rounded-lg">
+                                        <a href="index.php?action=cancel&id=<?php echo $task->getId(); ?>">
+                                            <i data-lucide="X" class="w-4 h-4 text-red-700 stroke-[4px]"></i>
+                                        </a>
+                                    </div>
+
+                                <?php else: ?>
+                                    <div class="border-2 border-gray-300 bg-gray-100 p-1 rounded-lg <?php echo ($task->getStatus()->value === 'done') ? 'text-green-500' : 'text-red-500' ?>">
+                                        <?php echo $task->getStatus()->value ?>
+                                    </div>
+                                <?php endif; ?>
+                                <div class="hover:bg-red-100 p-2 rounded-lg">
+                                    <a href="index.php?action=delete&id=<?php echo $task->getId(); ?>">
+                                        <i data-lucide="trash" class="w-4 h-4 text-black-700 stroke-[3px]"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex flex-col">
+                            <span class="text-xs text-gray-400"> Started: <?php echo $task->getCreatedAt()->format('M j, g:i a'); ?></span>
+                            <?php if ($task->getEndAt() !== null): ?>
+                                <span class="text-xs text-gray-400"> Completed: <?php echo $task->getCreatedAt()->format('M j, g:i a'); ?></span>
+                            <?php endif; ?>
                         </div>
                     </div>
                 <?php endforeach; ?>
