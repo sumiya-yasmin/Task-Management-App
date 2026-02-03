b5a89fdcfa88e0ef7d83e260954d17d69d7f4228
sumiya-yasmin <sumiyayasmin665@gmail.com>
2026-02-03 17:00:07 +0600

    Fix ui

--- src/Views/tasks.php
+++ src/Views/tasks.php
@@ -31,14 +31,12 @@
                 <?php foreach ($tasks as $task): ?>
                     <div class="flex flex-col gap-2 p-4 border rounded-lg hover:bg-gray-50 transition shadow-sm">
                         <div class="flex items-center justify-between gap-4">
-                            <div class="flex gap-4 items-center justify-center ">
-                                <span class="break-all" ><?php echo $task->getTitle(); ?></span>
+                            <div class="flex gap-4 items-center justify-between ">
+                                <span class="break-all"><?php echo htmlspecialchars($task->getTitle(), ENT_QUOTES, 'UTF-8'); ?></span>
                                 <?php if ($task->getStatus()->value === 'ongoing'): ?>
                                     <span class="border-2 border-gray-300 bg-gray-100 p-1 text-xs rounded-lg text-orange-300"> <?php echo $task->getStatus()->value ?> </span>
                                 <?php endif; ?>
                             </div>
-
-
 
                             <div class="flex justify-between gap-1">
                                 <?php if ($task->getStatus()->value === 'ongoing'): ?>
@@ -59,17 +57,16 @@
                                     </div>
                                 <?php endif; ?>
                                 <div class="hover:bg-red-100 p-2 rounded-lg">
-                                    <a href="index.php?action=delete&id=<?php echo $task->getId(); ?>"                                         onclick="return confirm('Are you sure, you want to delete?')"
->
+                                    <a href="index.php?action=delete&id=<?php echo $task->getId(); ?>" onclick="return confirm('Are you sure, you want to delete?')">
                                         <i data-lucide="trash" class="w-4 h-4 text-black-700 stroke-[3px]"></i>
                                     </a>
                                 </div>
                             </div>
                         </div>
-                        <div class="flex flex-col">
-                            <span class="text-xs text-gray-400"> Started: <?php echo $task->getCreatedAt()->format('M j, g:i a'); ?></span>
+                        <div class="flex flex-col gap-1">
+                            <span class="text-xs text-orange-300 rounded"> Started: <?php echo $task->getCreatedAt()->format('M j, g:i a'); ?></span>
                             <?php if ($task->getEndAt() !== null): ?>
-                                <span class="text-xs text-gray-400"> Completed: <?php echo $task->getEndAt()->format('M j, g:i a'); ?></span>
+                                <span class="text-xs text-blue-300 "> Completed: <?php echo $task->getEndAt()->format('M j, g:i a'); ?></span>
                             <?php endif; ?>
                         </div>
                     </div>
